cf1ffbd430b04f86b93ce2195d92563e6801eba3
Deniz Özen <denizgrad@hotmail.com>
2017-11-17 21:33:58 +0100

    Init&Config

---
+++ schedulerWeb/src/main/java/com/denizozen/scape/schedulerWeb/initializer/Initializer.java
@@ -0,0 +1,31 @@
+package com.denizozen.scape.schedulerWeb.initializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+/**
+ * 
+ * @author deniz.ozen
+ *
+ */
+public class Initializer implements WebApplicationInitializer {
+
+	public void onStartup(ServletContext servletContext)
+			throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(WebAppConfig.class);
+		servletContext.addListener(new ContextLoaderListener(ctx));
+
+		ctx.setServletContext(servletContext);
+
+		Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
+	}
+}
+
